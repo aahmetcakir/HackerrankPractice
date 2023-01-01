2e986ba57e6ceeff45a8da96933111546f96d876
ahmetcakir <ca.ahmetcakir@gmail.com>
2023-01-01 15:23:42 +0300

    DayOfTheProgrammer

---
+++ DayofTheProgrammer.js
@@ -0,0 +1,80 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'dayOfProgrammer' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER year as parameter.
+ */
+
+function dayOfProgrammer(year) {
+    // Write your code here
+    let date;
+    if (year > 1919) {
+        if (year % 400 === 0) {
+            const d = new Date(`01-01-${year}`)
+            d.setDate(d.getDate() + 255)
+            date = d.toLocaleDateString("tr-TR")
+            return date
+        } else {
+            const d = new Date(`01-01-${year}`)
+            d.setDate(d.getDate() + 255)
+            date = d.toLocaleDateString("tr-TR")
+            return date
+        }
+    } else {
+        if (year % 400 === 0 || year % 100 === 0) {
+            const d = new Date(`01-01-${year}`)
+            d.setDate(d.getDate() + 254)
+            date = d.toLocaleDateString("tr-TR")
+            return date
+        } else {
+            if (year === 1918) {
+                const d = new Date(`01-01-${year}`)
+                d.setDate(d.getDate() + 268)
+                date = d.toLocaleDateString("tr-TR")
+                return date
+            } else {
+                const d = new Date(`01-01-${year}`)
+                d.setDate(d.getDate() + 255)
+                date = d.toLocaleDateString("tr-TR")
+                return date
+            }
+
+        }
+    }
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const year = parseInt(readLine().trim(), 10);
+
+    const result = dayOfProgrammer(year);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
